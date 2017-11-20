9331620d53f897615abd8a1a04c8c78bdec7ad1e
Joseph Wang <wang.jo@husky.neu.edu>
2017-11-20 10:05:44 -0500

    Added sliders to quiz
    Added the slider questions to the quiz, along with the mindset select
    from options block. Still need to figure out the drag and drop element

--- src/javascripts/modules/quiz_vue.js
+++ src/javascripts/modules/quiz_vue.js
@@ -23,18 +23,69 @@ Vue.component('quiz-element', {
 			<button class="button primary" @click="startQuiz">Begin Quiz</button>
 		</div>
 
-		<div class="quiz-rate" v-else-if="step > 0 && step < rate_pages">
+		<div class="quiz-rate quiz-page" v-else-if="step > 0 && step <= questions.length">
 			<div class="questions-container">
-				<template v-for="q in questions[step-1]">
+				<template v-for="(q, index) in questions[step-1]">
 					<div class="quiz-question-block">
-						<p class="question">{{ q.question }}</p>
-						<div class="answer_list">
-							<template v-for="n in 6">
-								<div class="option" @click="q.answer=n" :class="{ selected: q.answer == n }"></div>
-							</template>
+						<p class="question">{{ q.question }}<span class="asterik" v-if="step === 1 && index === 0">* This question is required</span><span class="asterik" v-else>*</span>
+						</p>
+						<div class="response-container">
+							<div class="label small">Strongly Disagree</div>
+							<div class="answer_list">
+								<template v-for="n in 6">
+									<div class="option" @click="q.answer=n" :class="{ selected: q.answer == n }"></div>
+								</template>
+							</div>
+							<div class="label small">Strongly Agree</div>
 						</div>
 					</div>
 				</template>
+			</div>
+
+			<div class="action-container">
+				<button class="button" @click="stepChange(-1)">Previous</button>
+				<button class="button primary" @click="stepChange(1)">Next</button>
+			</div>
+		</div>
+
+		<div class="quiz-slide quiz-page" v-else-if="step === questions.length+1">
+			<div class="questions-container">
+				<p>Drag and drop to-do</p>
+			</div>
+			<div class="action-container">
+				<button class="button" @click="stepChange(-1)">Previous</button>
+				<button class="button primary" @click="stepChange(1)">Next</button>
+			</div>
+		</div>
+
+		<div class="quiz-slide quiz-page" v-else-if="step === questions.length+2">
+			<div class="questions-container">
+				<template v-for="q in slide_questions">
+					<div class="quiz-question-block">
+						<p class="question">{{ q.question }}<span class="asterik">*</span></p>
+						<div class="response-container">
+							<div class="label">Not at all true of me</div>
+							<div class="slider-container">
+								<input type="range" min="0" max="10" value="5" class="slider" v-model="q.answer">
+							</div>
+							<div class="label">Completley true of me</div>
+						</div>
+					</div>
+				</template>
+			</div>
+
+			<div class="questions-container">
+				<div class="select-wrapper">
+					<p class="question">{{ mindset_question.question }}<span class="asterik">*</span></p>
+					<div class="select_options">
+						<template v-for="(option, index) in mindset_question.options">
+							<div class="option" @click="mindset_question.answer=index" 
+							:class="{ selected: mindset_question.answer == index }"> 
+								<p class="text">{{ option }}</p>
+							</div>
+						</template>
+					</div>
+				</div>
 			</div>
 
 			<div class="action-container">
@@ -46,7 +97,6 @@ Vue.component('quiz-element', {
 	data: function() {
 		return {
 			step: 0,
-			rate_pages: 5,
 			questions: [
 				[
 					{
@@ -104,16 +154,44 @@ Vue.component('quiz-element', {
 						answer: null
 					},
 				]
-			]
+			],
+			slide_questions: [
+				{
+					question: "For the most part, I carry out the value of FIRST in various areas of my life",
+					answer: 5
+				},
+				{
+					question: "For the most part, I carry out the value of SECOND in various areas of my life",
+					answer: 5
+				},
+				{
+					question: "For the most part, I carry out the value of THIRD in various areas of my life",
+					answer: 5
+				}
+			],
+			mindset_question: {
+				question: "Choose the statement that best describes how you think",
+				options: [
+					"I stay stuck in my viewpoints",
+					"I change like the wind",
+					"I adjust my mindset and behaviors based on careful thinking"
+				],
+				answer: null
+			}
 		}
 	},
 	methods: {
 		startQuiz: function() {
 			this.step = 1;
+			Vue.nextTick(function(){
+				window.scroll(0, 0);
+			});
 		},
 		stepChange: function(diff) {
 			this.step += diff;
-			window.scroll(0, 70);
+			Vue.nextTick(function(){
+				window.scroll(0, 0);
+			});
 		},
 		submitQuiz: function() {
 			
