db2708954824f527aa0941d2392e0a0f52df7047
Joseph Wang <wang.jo@husky.neu.edu>
2017-12-07 19:24:43 -0500

    Dynamic Content For Press and Resources
    Set up the API for the Press and Resources entries, so that both are
    being loaded dynamically on the index page, resource page, and press
    page

---
+++ src/javascripts/modules/resources_vue.js
@@ -0,0 +1,107 @@
+// ---------------- Components Section ----------------
+
+// 4. Define the components to be used by the Vue instance
+// Each Vue component consists of the HTML tag that will be used for
+// it (Vue swaps it out for it's template during rendering) and its data
+
+// <home-feed>
+Vue.component('resources-grid', {
+	template: `<div id="resources">
+
+			<div class="top-select">
+				<ul class="category-list">
+					<li class="category active">Worksheets</li>
+					<li class="category">Presentations</li>
+					<li class="category">Videos</li>
+				</ul>
+			</div>
+
+			<div class="filter">
+				<input class="input" type="text" placeholder="Search for resources">
+				<button class="button dropdown sorter">
+					Sort By
+					<ul class="drop-menu">
+						<a class="buy-link"><li>Date Asc.</li></a>
+						<a class="buy-link"><li>Date Desc.</li></a>
+						<a class="buy-link"><li>Title Asc.</li></a>
+						<a class="buy-link"><li>Title Desc.</li></a>
+					</ul>
+				</button>
+			</div>
+
+			<div class="content">
+
+				<template v-for="entry in results">
+					<div class="resource_item">
+						<a v-bind:href="'./uploads/resources/'+ entry.filePath" download class="overlay_link"></a>
+						<div class="thumbnail" :class="'rot'+(getRandomNumber(1,4) * 90)">
+							<img class="img-ink" :src="'./static/images/inkblots/resource_ink/Resources_Icon' + getRandomNumber(1,4) + '.png'" >
+						</div>
+						<div class="info">
+							<p class="resource_title">{{ entry.title }}</p>
+							<p class="meta"><span class="file_type">.{{ entry.fileType.toUpperCase() }}</span> <span class="date">{{ parseDate(entry.date.date) }}</span></p>
+							<p class="more"><a class="link teal">download <span class="anim-arrow">→</span></a></p>
+						</div>
+					</div>
+				</template>
+				
+			</div>
+
+			<div class="action-container">
+				<button class="button black loader" @click="callAPI" :class="{ 'hidden': this.doneLoading }">load more</button>
+			</div>
+		</div>		`,
+	data: function() {
+		return {
+			pg: 1,
+			search: "", 
+			sortBy: null,
+			results: [],
+			doneLoading: false
+		}
+	},
+	methods: {
+		callAPI: function(data) {
+			var self = this;
+        	Vue.nextTick(function(){
+				$.ajax({
+					type: "GET",
+					url: "./api/resources.json?pg="+self.pg,
+					contentType: "application/json",
+					success: function(returnData) {
+						self.results = self.results.concat(returnData.data);
+						if (returnData.meta.pagination.total_pages == self.pg) {
+							self.doneLoading = true;
+						}
+						self.pg++;
+					},
+					error: function(jqXHR, textStatus, errorThrown) {
+						console.log(errorThrown);
+					}
+				});
+			});
+		},
+		getRandomNumber: function(min, max) {
+			min = Math.ceil(min);
+			max = Math.floor(max);
+			return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+		},
+		parseDate: function(datestring) {
+			var d = new Date(datestring);
+			return d.getMonth() + "/" + d.getDate() + "/" + d.getYear();
+		}
+	},
+	mounted: function() {
+		this.callAPI();
+	}
+});
+
+
+// ---------------- Parent Vue Section ----------------
+
+// 5. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+const vm = new Vue({
+    data: {}
+}).$mount('#content');
